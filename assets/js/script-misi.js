fbc9873decdab5993baacde392755058375a9c9d
trunap <121910477+trunap@users.noreply.github.com>
2026-08-14 19:51:52 +0700

    Update script-misi.js

--- assets/js/script-misi.js
+++ assets/js/script-misi.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 // ⚠️ GANTI DENGAN URL DEPLOYMENT BARUMU!
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyYNghkn3XZNfFXRzuWr0BSZf3EC8IZ80KoTO0llMMyouC5ozFfmV8dcAq2zOCTLW0Smg/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxm4eJGQjBytrLTQgYrsfEXIQxLQ_Rq7NFVM__Y8AhRfzPe8q5FJhofecqrDJ5ywkeBEg/exec"; 
 
 const VALID_PINS = ["a1b2c3", "v9t6c2", "123456"];
 let userPin = localStorage.getItem("AV_INVENTORY_PIN") || "";
